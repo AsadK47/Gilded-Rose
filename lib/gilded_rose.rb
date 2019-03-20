e813b09b5ed1713597906e2c0294f264305856d9
Asad Khan <asad_khan47@hotmail.co.uk>
2019-03-20 14:13:33 +0000

    Small change to make one line if statement

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -29,9 +29,7 @@ class GildedRose
       item.quality += 1
       item.quality += 1 if item.sell_in < 11 && item.quality < 50
       item.quality += 1 if item.sell_in < 6 && item.quality < 50
-      if item.sell_in == 0
-        item.quality = 0
-      end
+      item.sell_in == 0 ? item.quality = 0 : nil
     end
   end
 
