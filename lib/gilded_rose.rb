c0dd710ec079e271e563570af8331a8a1eb33cb7
Asad Khan <asad_khan47@hotmail.co.uk>
2019-03-20 10:59:45 +0000

    Rubocop -a automatic fixes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,7 +5,7 @@ class GildedRose
 
   def item_quality
     @items.each do |item|
-      item.sell_in -= 1 
+      item.sell_in -= 1
       item.quality -= 1 unless item.quality == 0
       if item.sell_in <= 0
         item.quality -= 1 unless item.quality == 0
@@ -16,6 +16,7 @@ class GildedRose
   def brie_quality
     @items.each do |item|
       return if item.quality >= 50
+
       item.quality += 1
       item.quality += 1 if item.sell_in <= 0 && item.quality < 48
     end
@@ -24,6 +25,7 @@ class GildedRose
   def backstage_pass_quality
     @items.each do |item|
       return if item.quality >= 50 && item.sell_in > 0
+
       item.quality += 1
       item.quality += 1 if item.sell_in < 11 && item.quality < 50
       item.quality += 1 if item.sell_in < 6 && item.quality < 50
